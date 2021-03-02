d668c661755135143b93bd42c31c0df6cd603415
Oskar Findahl <oskar_findahl@hotmail.com>
2021-03-02 15:57:40 +0100

    Ability to add new tasks and dialog function added

--- script.js
+++ script.js
@@ -6,4 +6,42 @@
     });
     
 
-});
+    let taskID = 1;
+    $(".create-task-button").on("click", function() {
+
+
+      var li = $('<li></li>').text('Task: ' + taskID);
+      var button = $('<button></button>').attr('type','button').attr('id',taskID).attr('class','dialog-button').text('Dialog');
+      var div = $('<div></div>').attr('class',taskID).attr('title','Basic dialog');
+      var p = $('<p></p>').text('Task ' + taskID + ' Dialog Info');
+      $('#sortable1').append(li);
+      li.append(button);
+      button.append(div);
+      div.append(p);
+
+
+  
+
+      $("." + taskID).dialog({
+        autoOpen: false,
+        show: {
+          effect: "blind",
+          duration: 200
+        },
+      });
+
+      taskID ++;
+
+      $( ".dialog-button" ).on( "click", function() {
+        let classId =  $(this).prop('id');
+        $("." + classId).dialog("open");
+
+      });
+
+    });
+      
+    
+    });
+  
+
+
